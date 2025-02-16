ecb33d5544caa91dc8e95b3c464f93f3bed65c0b
Hikanyan <hikanyan0131@gmail.com>
2025-02-16 21:26:29 +0900

    UI System

---
+++ Assets/HikanyanLaboratory/UISystem/PrefabProperty/PrefabReferenceAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace HikanyanLaboratory.UI
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class PrefabReferenceAttribute : PropertyAttribute
+    {
+        public string PrefabKey { get; private set; }
+
+        public PrefabReferenceAttribute(string prefabKey)
+        {
+            PrefabKey = prefabKey;
+        }
+    }
+}
